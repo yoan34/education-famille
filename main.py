f7cb69f7df79e6e201704d9d896e66c58e260cce
ybousquet <ybousquet@onestock-retail.com>
2023-07-10 14:09:31 +0200

    update data to test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('hello')
+
+data_test = {
+    "date": "06-06-2023",
+    "tonight": "PICK_NICK", # NO_PICK_NICK, LAETITIA_SOLO, YOAN_SOLO, NO_KIDS
+    "tonight_meal": "miam miam",
+    "parent_sleep_inducer": "YOAN", # LAETITIA
+    "inaya_bedtime": "22h",
+    "éliot_bedtime": "22h",
+    "walking_path": "",
+    "place": "Castanet-Tolosan",
+}
